2daa241f87d7c24c5880967175a085efaad309ef
yourrmomdotcom <122555214+yourrmomdotcom@users.noreply.github.com>
2023-01-13 12:25:18 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,3 +5,7 @@ input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Happy)
 })
 basic.showIcon(IconNames.Silly)
+basic.forever(function () {
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 5000, 1, 255, 255, 9999, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    music.setVolume(255)
+})
