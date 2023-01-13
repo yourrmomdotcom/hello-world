729f0812024ced89309e61a944e03d67ce47568b
yourrmomdotcom <122555214+yourrmomdotcom@users.noreply.github.com>
2023-01-13 12:05:13 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Chessboard)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Happy)
+})
+basic.showIcon(IconNames.Silly)
